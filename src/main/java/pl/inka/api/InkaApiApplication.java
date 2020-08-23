b2d2968aff275c591a6474607e973255db088aa9
Mateusz Łodziński <mateusz.lodzinski@7bulls.com>
2020-08-23 19:46:55 +0200

    added cors, added profiles and fixed tests

--- src/main/java/pl/inka/api/InkaApiApplication.java
+++ src/main/java/pl/inka/api/InkaApiApplication.java
@@ -1,14 +1,27 @@
 package pl.inka.api;
 
+import static java.util.Optional.ofNullable;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class InkaApiApplication {
 
 	public static void main(String[] args) {
+		setDefaultActiveSpringProfile();
 		SpringApplication.run(InkaApiApplication.class, args);
+	}
+
+	private static void setDefaultActiveSpringProfile() {
+		String activeProfile =
+				ofNullable(System.getenv("SPRING_PROFILES_ACTIVE"))
+						.orElseGet(() -> System.getProperty("spring.profiles.active"));
+
+		if (activeProfile == null) {
+			activeProfile = "dev";
+			System.setProperty("spring.profiles.active", activeProfile);
+		}
 	}
 
 }
--- src/test/java/pl/inka/api/InkaApiApplicationTests.java
+++ src/test/java/pl/inka/api/InkaApiApplicationTests.java
@@ -2,11 +2,13 @@ package pl.inka.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
+@SpringBootTest
+@ActiveProfiles("test")
 class InkaApiApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
